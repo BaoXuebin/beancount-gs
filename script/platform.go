f87a25d75ebdcde19e200d472d1b9dbb2d91629a
BaoXuebin <baoxbin@hotmail.com>
2021-12-15 22:39:07 +0800

    add: macOS open browser

--- script/platform.go
+++ script/platform.go
@@ -10,9 +10,20 @@ func isWindows() bool {
 	return os == "windows"
 }
 
+func isMacOS() bool {
+	os := runtime.GOOS
+	return os == "darwin"
+}
+
 func OpenBrowser(url string) {
 	if isWindows() {
 		cmd := exec.Command("cmd", "/C", "start", url)
+		err := cmd.Start()
+		if err != nil {
+			LogSystemError("Failed to open browser, error is " + err.Error())
+		}
+	} else if isMacOS() {
+		cmd := exec.Command("open", url)
 		err := cmd.Start()
 		if err != nil {
 			LogSystemError("Failed to open browser, error is " + err.Error())
